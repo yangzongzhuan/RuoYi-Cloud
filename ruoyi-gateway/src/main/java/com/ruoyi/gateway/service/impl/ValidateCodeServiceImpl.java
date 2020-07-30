41cf67da6dff20f68fb82988f16341e378f8d593
RuoYi <yzz_ivy@163.com>
2020-07-30 18:26:52 +0800

    验证码类型支持（数组计算、字符验证）

--- ruoyi-gateway/src/main/java/com/ruoyi/gateway/service/impl/ValidateCodeServiceImpl.java
+++ ruoyi-gateway/src/main/java/com/ruoyi/gateway/service/impl/ValidateCodeServiceImpl.java
@@ -3,6 +3,7 @@ package com.ruoyi.gateway.service.impl;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import javax.annotation.Resource;
 import javax.imageio.ImageIO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,11 +26,17 @@ import com.ruoyi.gateway.service.ValidateCodeService;
 @Service
 public class ValidateCodeServiceImpl implements ValidateCodeService
 {
-    @Autowired
-    private Producer producer;
+    @Resource(name = "captchaProducer")
+    private Producer captchaProducer;
+
+    @Resource(name = "captchaProducerMath")
+    private Producer captchaProducerMath;
 
     @Autowired
     private RedisService redisService;
+
+    // 验证码类型
+    private String captchaType = "math";
 
     /**
      * 生成验证码
@@ -37,16 +44,28 @@ public class ValidateCodeServiceImpl implements ValidateCodeService
     @Override
     public AjaxResult createCapcha() throws IOException, CaptchaException
     {
-        // 生成验证码
-        String capText = producer.createText();
-        String capStr = capText.substring(0, capText.lastIndexOf("@"));
-        String verifyCode = capText.substring(capText.lastIndexOf("@") + 1);
-        BufferedImage image = producer.createImage(capStr);
         // 保存验证码信息
         String uuid = IdUtils.simpleUUID();
         String verifyKey = Constants.CAPTCHA_CODE_KEY + uuid;
 
-        redisService.setCacheObject(verifyKey, verifyCode, Constants.CAPTCHA_EXPIRATION, TimeUnit.MINUTES);
+        String capStr = null, code = null;
+        BufferedImage image = null;
+
+        // 生成验证码
+        if ("math".equals(captchaType))
+        {
+            String capText = captchaProducerMath.createText();
+            capStr = capText.substring(0, capText.lastIndexOf("@"));
+            code = capText.substring(capText.lastIndexOf("@") + 1);
+            image = captchaProducerMath.createImage(capStr);
+        }
+        else if ("char".equals(captchaType))
+        {
+            capStr = code = captchaProducer.createText();
+            image = captchaProducer.createImage(capStr);
+        }
+
+        redisService.setCacheObject(verifyKey, code, Constants.CAPTCHA_EXPIRATION, TimeUnit.MINUTES);
         // 转换流信息写出
         FastByteArrayOutputStream os = new FastByteArrayOutputStream();
         try
