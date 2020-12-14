62d7ce5aa9ed49c8f12b5b8a34004b9530c89d38
isme <1219353840@qq.com>
2020-12-14 14:38:31 +0800

    修复代码生成路径错误

--- ruoyi-ui/src/views/tool/gen/index.vue
+++ ruoyi-ui/src/views/tool/gen/index.vue
@@ -257,7 +257,7 @@ export default {
           this.msgSuccess("成功生成到自定义路径：" + row.genPath);
         });
       } else {
-        downLoadZip("/tool/gen/batchGenCode?tables=" + tableNames, "ruoyi");
+        downLoadZip("/code/gen/batchGenCode?tables=" + tableNames, "ruoyi");
       }
     },
     /** 同步数据库操作 */
