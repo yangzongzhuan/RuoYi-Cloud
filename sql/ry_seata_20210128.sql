63d7e816ff8058c0abaf4be05e576e0436a12547
RuoYi <yzz_ivy@163.com>
2021-06-02 09:58:36 +0800

    添加docker支持

--- sql/ry_seata_20210128.sql
+++ sql/ry_seata_20210128.sql
@@ -1,3 +1,12 @@
+DROP DATABASE IF EXISTS `ry-seata`;
+
+CREATE DATABASE  `ry-seata` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+USE `ry-seata`;
+
 -- -------------------------------- The script used when storeMode is 'db' --------------------------------
 -- the table to store GlobalSession data
 CREATE TABLE IF NOT EXISTS `global_table`
