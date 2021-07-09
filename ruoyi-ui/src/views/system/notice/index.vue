ee7607bccaa8d2b60a3b6c6e8bdee36eda41be62
RuoYi <yzz_ivy@163.com>
2021-07-09 21:12:37 +0800

    全局注册通用组件

--- ruoyi-ui/src/views/system/notice/index.vue
+++ ruoyi-ui/src/views/system/notice/index.vue
@@ -176,14 +176,10 @@
 </template>
 
 <script>
-import { listNotice, getNotice, delNotice, addNotice, updateNotice, exportNotice } from "@/api/system/notice";
-import Editor from '@/components/Editor';
+import { listNotice, getNotice, delNotice, addNotice, updateNotice } from "@/api/system/notice";
 
 export default {
   name: "Notice",
-  components: {
-    Editor
-  },
   data() {
     return {
       // 遮罩层
