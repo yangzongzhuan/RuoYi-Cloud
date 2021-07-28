12209ae5a4df097f73c48d149c7fed799286a5d3
RuoYi <yzz_ivy@163.com>
2021-07-28 13:17:20 +0800

    升级spring-boot到最新版本2.5.3

--- ruoyi-modules/ruoyi-system/src/main/java/com/ruoyi/system/controller/SysUserController.java
+++ ruoyi-modules/ruoyi-system/src/main/java/com/ruoyi/system/controller/SysUserController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -219,7 +218,8 @@ public class SysUserController extends BaseController
     @DeleteMapping("/{userIds}")
     public AjaxResult remove(@PathVariable Long[] userIds)
     {
-        if (ArrayUtils.contains(userIds, SecurityUtils.getUserId())) {
+        if (ArrayUtils.contains(userIds, SecurityUtils.getUserId()))
+        {
             return AjaxResult.error("当前用户不能删除");
         }
         return toAjax(userService.deleteUserByIds(userIds));
