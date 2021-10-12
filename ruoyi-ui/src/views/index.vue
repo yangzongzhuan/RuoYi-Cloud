af1b557bc8db65b45390aa4895fe2b6fc3445b28
RuoYi <yzz_ivy@163.com>
2021-10-12 09:01:05 +0800

    若依 3.2.0

--- ruoyi-ui/src/views/index.vue
+++ ruoyi-ui/src/views/index.vue
@@ -146,6 +146,55 @@
             <span>更新日志</span>
           </div>
           <el-collapse accordion>
+            <el-collapse-item title="v3.2.0 - 2021-10-12">
+              <ol>
+                <li>菜单管理支持配置路由参数</li>
+                <li>定时任务支持在线生成cron表达式</li>
+                <li>自定义弹层溢出滚动样式</li>
+                <li>自定义可拖动弹窗宽度指令</li>
+                <li>自定义可拖动弹窗高度指令</li>
+                <li>修改时检查用户数据权限范围</li>
+                <li>修复保存配置主题颜色失效问题</li>
+                <li>新增暗色菜单风格主题</li>
+                <li>菜单&部门新增展开/折叠功能</li>
+                <li>页签新增关闭左侧&添加图标</li>
+                <li>代码生成主子表多选行数据</li>
+                <li>日期范围支持添加多组</li>
+                <li>Excel导入支持@Excels注解</li>
+                <li>Excel注解支持导入导出标题信息</li>
+                <li>Excel注解支持自定义数据处理器</li>
+                <li>日志注解新增是否保存响应参数</li>
+                <li>定时任务对检查异常进行事务回滚</li>
+                <li>补充定时任务表字段注释</li>
+                <li>定时任务屏蔽ldap远程调用</li>
+                <li>新增通用方法简化下载使用</li>
+                <li>新增通用方法简化模态/缓存使用</li>
+                <li>新增data-dict组件简化数据字典使用</li>
+                <li>禁用dict-tag组件的渐变动画</li>
+                <li>默认首页使用keep-alive缓存</li>
+                <li>升级springcloud到最新版2020.0.4</li>
+                <li>升级spring-boot到最新版本2.5.5</li>
+                <li>升级spring-boot-admin到最新版2.5.2</li>
+                <li>升级pagehelper到最新版1.4.0</li>
+                <li>升级fastjson到最新版1.2.78</li>
+                <li>升级druid到最新版1.2.8</li>
+                <li>升级element-ui到最新版本2.15.6</li>
+                <li>升级sass-loader到最新版本10.1.1</li>
+                <li>升级dart-sass到版本1.32.13</li>
+                <li>升级file-saver到最新版本2.0.5</li>
+                <li>优化异常处理信息</li>
+                <li>验证码默认20s超时</li>
+                <li>优化代码生成导入表按创建时间排序</li>
+                <li>优化代码生成点击预览重置激活tab</li>
+                <li>修复主子表代码模板方法名错误问题</li>
+                <li>修复xss过滤后格式出现的异常</li>
+                <li>修复多图组件验证失败被删除问题</li>
+                <li>请求参数新增reasonable分页合理化属性</li>
+                <li>修复代码生成页面数据编辑保存之后总是跳转第一页的问题</li>
+                <li>修复带safari浏览器无法格式化utc日期格式yyyy-MM-dd'T'HH:mm:ss.SSS问题</li>
+                <li>其他细节优化</li>
+              </ol>
+            </el-collapse-item>
             <el-collapse-item title="v3.1.0 - 2021-08-02">
               <ol>
                 <li>支持配置XSS跨站脚本过滤</li>
@@ -560,7 +609,7 @@ export default {
   data() {
     return {
       // 版本号
-      version: "3.1.0",
+      version: "3.2.0",
     };
   },
   methods: {
