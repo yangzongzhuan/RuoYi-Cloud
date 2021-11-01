e165901506650822922cd2b84364f64dfe3f12c8
RuoYi <yzz_ivy@163.com>
2021-11-01 15:44:42 +0800

    登录/验证码请求headers不设置token

--- ruoyi-ui/src/api/login.js
+++ ruoyi-ui/src/api/login.js
@@ -4,6 +4,9 @@ import request from '@/utils/request'
 export function login(username, password, code, uuid) {
   return request({
     url: '/auth/login',
+    headers: {
+      isToken: false
+    },
     method: 'post',
     data: { username, password, code, uuid }
   })
@@ -49,6 +52,9 @@ export function logout() {
 export function getCodeImg() {
   return request({
     url: '/code',
+    headers: {
+      isToken: false
+    },
     method: 'get',
     timeout: 20000
   })
