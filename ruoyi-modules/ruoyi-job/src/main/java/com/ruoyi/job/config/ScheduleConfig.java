08885f65cd6815fbc18a5b485eb9fd99c2a0d2cf
RuoYi <yzz_ivy@163.com>
2021-11-24 16:40:18 +0800

    升级velocity到最新版本2.3

--- ruoyi-modules/ruoyi-job/src/main/java/com/ruoyi/job/config/ScheduleConfig.java
+++ ruoyi-modules/ruoyi-job/src/main/java/com/ruoyi/job/config/ScheduleConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 /**
- * 定时任务配置
+ * 定时任务配置（单机部署建议删除此类和qrtz数据库表，默认走内存会最高效）
  * 
  * @author ruoyi
  */
