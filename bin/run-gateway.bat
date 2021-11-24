961825d25cecd6ad84b9ae92c28d489c35b30484
RuoYi <yzz_ivy@163.com>
2021-11-24 16:39:55 +0800

    update bin

--- bin/run-gateway.bat
+++ bin/run-gateway.bat
@@ -8,7 +8,7 @@ cd ../ruoyi-gateway/target
 
 set JAVA_OPTS=-Xms512m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m
 
-java -Dfile.encoding=utf-8 -jar %JAVA_OPTS% ruoyi-gateway.jar
+java -Dfile.encoding=utf-8 %JAVA_OPTS% -jar ruoyi-gateway.jar
 
 cd bin
 pause
--- bin/run-monitor.bat
+++ bin/run-monitor.bat
@@ -8,7 +8,7 @@ cd ../ruoyi-visual/ruoyi-monitor/target
 
 set JAVA_OPTS=-Xms512m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m
 
-java -Dfile.encoding=utf-8 -jar %JAVA_OPTS% ruoyi-visual-monitor.jar
+java -Dfile.encoding=utf-8 %JAVA_OPTS% -jar ruoyi-visual-monitor.jar
 
 cd bin
 pause
