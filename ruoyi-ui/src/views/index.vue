3c7018b38ace22cc8b55217864640ce5ada4dcee
RuoYi <yzz_ivy@163.com>
2021-12-13 09:02:33 +0800

    若依 3.3.0

--- ruoyi-ui/src/views/index.vue
+++ ruoyi-ui/src/views/index.vue
@@ -146,6 +146,39 @@
             <span>更新日志</span>
           </div>
           <el-collapse accordion>
+            <el-collapse-item title="v3.3.0 - 2021-12-13">
+              <ol>
+                <li>新增配套并同步的Vue3前端版本</li>
+                <li>新增认证对象简化权限验证</li>
+                <li>新增tab对象简化页签操作</li>
+                <li>修改获取缓存信息方式</li>
+                <li>修改权限认证注解实现</li>
+				<li>自定义文字复制剪贴指令</li>
+                <li>升级axios到最新版本0.24.0</li>
+                <li>升级core-js到最新版本3.19.1</li>
+                <li>升级jsencrypt到最新版本3.2.1</li>
+                <li>升级js-cookie到最新版本3.0.1</li>
+				<li>升级clipboard到最新版本2.0.8</li>
+                <li>升级velocity到最新版本2.3</li>
+                <li>升级spring-boot到最新版本2.5.6</li>
+                <li>升级spring-boot-admin到最新版2.5.4</li>
+                <li>升级dynamic-ds到最新版本3.5.0</li>
+				<li>代码生成预览支持复制内容</li>
+                <li>修复五级以上菜单出现的404问题</li>
+                <li>生产环境使用路由懒加载提升页面响应速度</li>
+                <li>任务屏蔽违规字符&参数忽略双引号中的逗号</li>
+                <li>优化用户个人信息接口防止修改用户名</li>
+                <li>优化登录/验证码请求headers不设置token</li>
+                <li>优化注册成功提示消息类型success</li>
+                <li>优化下载解析blob响应是否登录失效</li>
+                <li>修复字符串无法被反转义问题</li>
+                <li>修复响应体过大出现的乱码问题</li>
+                <li>修复回显数据字典组的键值错误</li>
+                <li>修复代码生成复选框字典遗漏问题</li>
+                <li>修复代码生成模板主子表删除缺少事务</li>
+                <li>其他细节优化</li>
+              </ol>
+            </el-collapse-item>
             <el-collapse-item title="v3.2.0 - 2021-10-12">
               <ol>
                 <li>菜单管理支持配置路由参数</li>
@@ -609,7 +642,7 @@ export default {
   data() {
     return {
       // 版本号
-      version: "3.2.0",
+      version: "3.3.0",
     };
   },
   methods: {
