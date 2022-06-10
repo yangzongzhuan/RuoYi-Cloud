8add8efaf562e1c1b9b20df2f1599b7863c9d91c
Emil.Zhang <356141959@qq.com>
2022-06-10 18:04:54 +0800

    feat(优化部署):
    1. 优化部署时的步骤
    2. 解决Idea使用MybatisX插件时实体类报红的问题

--- docker/copy.sh
+++ docker/copy.sh
@@ -2,40 +2,116 @@
 
 # 复制项目的文件到对应docker路径，便于一键生成镜像。
 usage() {
-	echo "Usage: sh copy.sh"
-	exit 1
+  echo "Usage: sh copy.sh [copy|clear] [sql|web|jar|all]"
+  exit 1
 }
 
+# 复制
+copyFc() {
+  case "$1" in
+  "sql")
+    copySql
+    ;;
+  "web")
+    copyWeb
+    ;;
+  "jar")
+    copyJar
+    ;;
+  "all")
+    copySql
+    copyWeb
+    copyJar
+    ;;
+  *)
+    usage
+    ;;
+  esac
+}
 
-# copy sql
-echo "begin copy sql "
-cp ../sql/ry_20210908.sql ./mysql/db
-cp ../sql/ry_config_20220114.sql ./mysql/db
+copySql() {
+  echo "begin copy sql "
+  cp ../sql/ry_*.sql ./mysql/db
+}
 
-# copy html
-echo "begin copy html "
-cp -r ../ruoyi-ui/dist/** ./nginx/html/dist
+copyWeb() {
+  echo "begin copy html "
+  cp -r ../ruoyi-ui/dist/** ./nginx/html/dist
+}
 
+copyJar() {
+  echo "begin copy ruoyi-gateway "
+  cp ../ruoyi-gateway/target/ruoyi-gateway.jar ./ruoyi/gateway/jar
 
-# copy jar
-echo "begin copy ruoyi-gateway "
-cp ../ruoyi-gateway/target/ruoyi-gateway.jar ./ruoyi/gateway/jar
+  echo "begin copy ruoyi-auth "
+  cp ../ruoyi-auth/target/ruoyi-auth.jar ./ruoyi/auth/jar
 
-echo "begin copy ruoyi-auth "
-cp ../ruoyi-auth/target/ruoyi-auth.jar ./ruoyi/auth/jar
+  echo "begin copy ruoyi-visual "
+  cp ../ruoyi-visual/ruoyi-monitor/target/ruoyi-visual-monitor.jar ./ruoyi/visual/monitor/jar
 
-echo "begin copy ruoyi-visual "
-cp ../ruoyi-visual/ruoyi-monitor/target/ruoyi-visual-monitor.jar  ./ruoyi/visual/monitor/jar
+  echo "begin copy ruoyi-modules-system "
+  cp ../ruoyi-modules/ruoyi-system/target/ruoyi-modules-system.jar ./ruoyi/modules/system/jar
 
-echo "begin copy ruoyi-modules-system "
-cp ../ruoyi-modules/ruoyi-system/target/ruoyi-modules-system.jar ./ruoyi/modules/system/jar
+  echo "begin copy ruoyi-modules-file "
+  cp ../ruoyi-modules/ruoyi-file/target/ruoyi-modules-file.jar ./ruoyi/modules/file/jar
 
-echo "begin copy ruoyi-modules-file "
-cp ../ruoyi-modules/ruoyi-file/target/ruoyi-modules-file.jar ./ruoyi/modules/file/jar
+  echo "begin copy ruoyi-modules-job "
+  cp ../ruoyi-modules/ruoyi-job/target/ruoyi-modules-job.jar ./ruoyi/modules/job/jar
 
-echo "begin copy ruoyi-modules-job "
-cp ../ruoyi-modules/ruoyi-job/target/ruoyi-modules-job.jar ./ruoyi/modules/job/jar
+  echo "begin copy ruoyi-modules-gen "
+  cp ../ruoyi-modules/ruoyi-gen/target/ruoyi-modules-gen.jar ./ruoyi/modules/gen/jar
+}
 
-echo "begin copy ruoyi-modules-gen "
-cp ../ruoyi-modules/ruoyi-gen/target/ruoyi-modules-gen.jar ./ruoyi/modules/gen/jar
+# 清除
+clearFc() {
+  case "$1" in
+  "sql")
+    clearSql
+    ;;
+  "web")
+    clearWeb
+    ;;
+  "jar")
+    clearJar
+    ;;
+  "all")
+    clearSql
+    clearWeb
+    clearJar
+    ;;
+  *)
+    usage
+    ;;
+  esac
+}
 
+clearSql() {
+  echo "Clean sql data"
+  rm -rf ./mysql/db/*.sql
+}
+
+clearWeb() {
+  echo "Clean web data"
+  rm -rf ./nginx/html/dist/*
+}
+
+clearJar() {
+  echo "Clean jar data"
+  rm -rf ./ruoyi/gateway/jar/*.jar
+  rm -rf ./ruoyi/auth/jar/*.jar
+  rm -rf ./ruoyi/visual/monitor/jar/*.jar
+  rm -rf ./ruoyi/modules/*/jar/*.jar
+}
+
+# 根据输入参数，选择执行对应方法，不输入则执行使用说明
+case "$1" in
+"clear")
+  clearFc "$2"
+  ;;
+"copy")
+  copyFc "$2"
+  ;;
+*)
+  usage
+  ;;
+esac
--- docker/deploy.sh
+++ docker/deploy.sh
@@ -2,66 +2,74 @@
 
 # 使用说明，用来提示输入参数
 usage() {
-	echo "Usage: sh 执行脚本.sh [port|base|modules|stop|rm]"
-	exit 1
+  echo "Usage: sh 执行脚本.sh [port|base|modules|rebuild|stop|rm]"
+  exit 1
 }
 
 # 开启所需端口
-port(){
-	firewall-cmd --add-port=80/tcp --permanent
-	firewall-cmd --add-port=8080/tcp --permanent
-	firewall-cmd --add-port=8848/tcp --permanent
-	firewall-cmd --add-port=9848/tcp --permanent
-	firewall-cmd --add-port=9849/tcp --permanent
-	firewall-cmd --add-port=6379/tcp --permanent
-	firewall-cmd --add-port=3306/tcp --permanent
-	firewall-cmd --add-port=9100/tcp --permanent
-	firewall-cmd --add-port=9200/tcp --permanent
-	firewall-cmd --add-port=9201/tcp --permanent
-	firewall-cmd --add-port=9202/tcp --permanent
-	firewall-cmd --add-port=9203/tcp --permanent
-	firewall-cmd --add-port=9300/tcp --permanent
-	service firewalld restart
+port() {
+  firewall-cmd --add-port=80/tcp --permanent
+  firewall-cmd --add-port=8080/tcp --permanent
+  firewall-cmd --add-port=8848/tcp --permanent
+  firewall-cmd --add-port=9848/tcp --permanent
+  firewall-cmd --add-port=9849/tcp --permanent
+  firewall-cmd --add-port=6379/tcp --permanent
+  firewall-cmd --add-port=3306/tcp --permanent
+  firewall-cmd --add-port=9100/tcp --permanent
+  firewall-cmd --add-port=9200/tcp --permanent
+  firewall-cmd --add-port=9201/tcp --permanent
+  firewall-cmd --add-port=9202/tcp --permanent
+  firewall-cmd --add-port=9203/tcp --permanent
+  firewall-cmd --add-port=9300/tcp --permanent
+  service firewalld restart
 }
 
 # 启动基础环境（必须）
-base(){
-	docker-compose up -d ruoyi-mysql ruoyi-redis ruoyi-nacos
+base() {
+  docker-compose up -d ruoyi-mysql ruoyi-redis ruoyi-nacos
 }
 
 # 启动程序模块（必须）
-modules(){
-	docker-compose up -d ruoyi-nginx ruoyi-gateway ruoyi-auth ruoyi-modules-system
+modules() {
+  docker-compose up -d ruoyi-nginx ruoyi-gateway ruoyi-auth ruoyi-modules-system
+}
+
+# 重新打包并发布
+reBuild() {
+  docker-compose up -d --build ruoyi-gateway ruoyi-auth ruoyi-modules-system
 }
 
 # 关闭所有环境/模块
-stop(){
-	docker-compose stop
+stop() {
+  docker-compose stop
 }
 
 # 删除所有环境/模块
-rm(){
-	docker-compose rm
+rm() {
+  docker-compose rm
 }
 
 # 根据输入参数，选择执行对应方法，不输入则执行使用说明
 case "$1" in
 "port")
-	port
-;;
+  port
+  ;;
 "base")
-	base
-;;
+  base
+  ;;
 "modules")
-	modules
-;;
+  modules
+  ;;
+"rebuild")
+  reBuild
+  ;;
 "stop")
-	stop
-;;
+  stop
+  ;;
 "rm")
-	rm
-;;
+  rm
+  ;;
 *)
-	usage
-;;
+  usage
+  ;;
 esac
