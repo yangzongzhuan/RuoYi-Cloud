5b8cf9701fa1324936b4f92d8c627c749d2a8b44
光速蜗牛 <361946749@qq.com>
2022-06-13 10:12:38 +0000

    update ruoyi-gateway/src/main/java/com/ruoyi/gateway/filter/XssFilter.java. update 优化 XssFilter 使用枚举替换字符串

--- ruoyi-gateway/src/main/java/com/ruoyi/gateway/filter/XssFilter.java
+++ ruoyi-gateway/src/main/java/com/ruoyi/gateway/filter/XssFilter.java
@@ -44,7 +44,7 @@ public class XssFilter implements GlobalFilter, Ordered
         ServerHttpRequest request = exchange.getRequest();
         // GET DELETE 不过滤
         HttpMethod method = request.getMethod();
-        if (method == null || method.matches("GET") || method.matches("DELETE"))
+        if (method == null || method == HttpMethod.GET || method == HttpMethod.DELETE)
         {
             return chain.filter(exchange);
         }
