4ae26c2f8676ba73f1e17932ea03e7d581bd82af
runphp <runphp@qq.com>
2022-08-04 01:39:58 +0000

    新增删除Hash中的某条数据

--- ruoyi-common/ruoyi-common-redis/src/main/java/com/ruoyi/common/redis/service/RedisService.java
+++ ruoyi-common/ruoyi-common-redis/src/main/java/com/ruoyi/common/redis/service/RedisService.java
@@ -244,6 +244,18 @@ public class RedisService
     }
 
     /**
+     * 删除Hash中的某条数据
+     *
+     * @param key Redis键
+     * @param hKey  Hash键
+     * @return 是否成功
+     */
+    public boolean deleteCacheMapValue(final String key, final String hKey)
+    {
+        return Boolean.TRUE.equals(redisTemplate.opsForHash().delete(key, hKey));
+    }
+
+    /**
      * 获得缓存的基本对象列表
      * 
      * @param pattern 字符串前缀
