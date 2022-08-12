59b867dcca385a5ec5b64699f0ae3f94ff769061
RuoYi <yzz_ivy@163.com>
2022-08-12 12:19:42 +0800

    限制用户个人信息修改部门

--- ruoyi-modules/ruoyi-system/src/main/java/com/ruoyi/system/controller/SysProfileController.java
+++ ruoyi-modules/ruoyi-system/src/main/java/com/ruoyi/system/controller/SysProfileController.java
@@ -82,6 +82,7 @@ public class SysProfileController extends BaseController
         user.setUserId(sysUser.getUserId());
         user.setPassword(null);
         user.setAvatar(null);
+        user.setDeptId(null);
         if (userService.updateUserProfile(user) > 0)
         {
             // 更新缓存用户信息
