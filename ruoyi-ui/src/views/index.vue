c2708f2ba4f3da914580ed2638fa84403b10226d
RuoYi <yzz_ivy@163.com>
2022-09-30 11:48:39 +0800

    若依 3.6.1

--- ruoyi-ui/src/views/index.vue
+++ ruoyi-ui/src/views/index.vue
@@ -146,6 +146,50 @@
             <span>更新日志</span>
           </div>
           <el-collapse accordion>
+            <el-collapse-item title="v3.6.1 - 2022-10-01">
+              <ol>
+                <li>数据逻辑删除不进行唯一验证</li>
+                <li>支持多权限字符匹配角色数据权限</li>
+                <li>页面内嵌iframe切换tab不刷新数据</li>
+                <li>新增密码最大错误次数/锁定时间</li>
+                <li>登录日志新增解锁账户功能</li>
+                <li>通用下载方法新增config配置选项</li>
+                <li>操作日志记录支持排除敏感属性字段</li>
+                <li>Excel注解支持导出对象的子列表方法</li>
+                <li>Excel注解支持自定义隐藏属性列</li>
+                <li>Excel注解支持backgroundColor属性设置背景色</li>
+                <li>升级spring-cloud-alibaba到最新版2021.0.4.0</li>
+                <li>升级spring-cloud到最新版2021.0.4</li>
+                <li>升级spring-boot到最新版本2.7.3</li>
+                <li>升级spring-boot-admin到最新版2.7.5</li>
+                <li>升级seata到最新版1.5.2</li>
+                <li>升级druid到最新版本1.2.12</li>
+                <li>升级fastjson到最新版2.0.14</li>
+                <li>升级pagehelper到最新版1.4.5</li>
+                <li>升级core-js到最新版本3.25.2</li>
+                <li>升级dynamic-ds到最新版本3.5.2</li>
+                <li>升级element-ui到最新版本2.15.10</li>
+                <li>修复多文件上传报错出现的异常问题</li>
+                <li>修复图片预览组件src属性为null值控制台报错问题</li>
+                <li>修复使用FastDFS上传头像失败提示文件名没有后缀问题</li>
+                <li>优化seata单独依赖模块</li>
+                <li>优化任务过期不执行调度</li>
+                <li>优化字典数据使用store存取</li>
+                <li>优化代码生成同步后值NULL问题</li>
+                <li>优化定时任务支持执行父类方法</li>
+                <li>优化修改资料头像被覆盖的问题</li>
+                <li>优化修改用户登录账号重复验证</li>
+                <li>优化用户个人信息接口防止修改部门</li>
+                <li>优化布局设置使用el-drawer抽屉显示</li>
+                <li>优化日志注解记录限制请求地址的长度</li>
+                <li>优化导入更新用户数据前校验数据权限</li>
+                <li>优化excel/scale属性导出单元格数值类型</li>
+                <li>优化日志操作中重置按钮时重复查询的问题</li>
+                <li>优化多个相同角色数据导致权限SQL重复问题</li>
+                <li>优化表格上右侧工具条（搜索按钮显隐&右侧样式凸出）</li>
+                <li>其他细节优化</li>
+              </ol>
+            </el-collapse-item>
             <el-collapse-item title="v3.6.0 - 2022-07-16">
               <ol>
                 <li>Excel注解支持color字体颜色</li>
@@ -754,7 +798,7 @@ export default {
   data() {
     return {
       // 版本号
-      version: "3.6.0",
+      version: "3.6.1",
     };
   },
   methods: {
