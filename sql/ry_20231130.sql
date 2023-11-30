d0e9e07e9ca4ef40ac20b12a4410129adb025588
RuoYi <yzz_ivy@163.com>
2023-11-30 09:46:27 +0800

    代码生成支持选择前端模板类型

--- sql/ry_20231130.sql
+++ sql/ry_20231130.sql
@@ -647,6 +647,7 @@ create table gen_table (
   sub_table_fk_name varchar(64)     default null               comment '子表关联的外键名',
   class_name        varchar(100)    default ''                 comment '实体类名称',
   tpl_category      varchar(200)    default 'crud'             comment '使用的模板（crud单表操作 tree树表操作）',
+  tpl_web_type      varchar(30)     default ''                 comment '前端模板类型（element-ui模版 element-plus模版）',
   package_name      varchar(100)                               comment '生成包路径',
   module_name       varchar(30)                                comment '生成模块名',
   business_name     varchar(30)                                comment '生成业务名',
