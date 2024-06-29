2a73de34f0b82133656fd49f1a9a4916b0030c38
RuoYi <yzz_ivy@163.com>
2024-06-29 22:45:51 +0800

    优化代码

--- ruoyi-ui/src/views/system/role/selectUser.vue
+++ ruoyi-ui/src/views/system/role/selectUser.vue
@@ -127,10 +127,8 @@ export default {
       }
       authUserSelectAll({ roleId: roleId, userIds: userIds }).then(res => {
         this.$modal.msgSuccess(res.msg);
-        if (res.code === 200) {
-          this.visible = false;
-          this.$emit("ok");
-        }
+        this.visible = false;
+        this.$emit("ok");
       });
     }
   }
