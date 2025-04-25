5f3a93d12eda9344f2bfa0208f5fe97344eb60e2
RuoYi <yzz_ivy@163.com>
2025-04-25 15:12:03 +0800

    优化代码

--- ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/core/utils/poi/ExcelUtil.java
+++ ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/core/utils/poi/ExcelUtil.java
@@ -972,7 +972,8 @@ public class ExcelUtil<T>
     /**
      * 添加单元格
      */
-    public Cell addCell(Excel attr, Row row, T vo, Field field, int column)
+    @SuppressWarnings("deprecation")
+	public Cell addCell(Excel attr, Row row, T vo, Field field, int column)
     {
         Cell cell = null;
         try
