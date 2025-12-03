a3eefb6bad993eb0bc306f085885cf99a8892878
RuoYi <yzz_ivy@163.com>
2025-12-03 10:27:23 +0800

    优化生成代码下载的zip文件名

--- ruoyi-ui/src/views/tool/gen/index.vue
+++ ruoyi-ui/src/views/tool/gen/index.vue
@@ -253,7 +253,8 @@ export default {
           this.$modal.msgSuccess("成功生成到自定义路径：" + row.genPath)
         })
       } else {
-        this.$download.zip("/code/gen/batchGenCode?tables=" + tableNames, "ruoyi.zip")
+        const zipName = Array.isArray(tableNames) ? "ruoyi.zip" : tableNames + ".zip"
+        this.$download.zip("/code/gen/batchGenCode?tables=" + tableNames, zipName)
       }
     },
     /** 同步数据库操作 */
