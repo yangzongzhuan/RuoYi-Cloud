1ba4c986432f1e44dfd686e15a1efef70b0217bd
jerry_su <136738189@qq.com>
2026-04-15 17:02:39 +0800

    refactor(CustomerController): 移除不必要的PreAuthorize注解

--- ruoyi-modules/ruoyi-application/src/main/java/com/ruoyi/application/controller/CustomerController.java
+++ ruoyi-modules/ruoyi-application/src/main/java/com/ruoyi/application/controller/CustomerController.java
@@ -3,7 +3,6 @@ package com.ruoyi.application.controller;
 import java.util.List;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
